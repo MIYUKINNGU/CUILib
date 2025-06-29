05b26ebaef9b27c0f581491e7a40337df2258f5d
MIYUKINNGU <111403555+MIYUKINNGU@users.noreply.github.com>
2025-06-29 18:53:53 +0900

    Update Error.py

--- Error.py
+++ Error.py
@@ -3,6 +3,3 @@ class InitializeError(Exception):
 
 class PropertyError(Exception):
     pass
-
-def InstantiateValidError(T: type):
-    return SyntaxError(f"It is not valid to instantiate {T.__name__} class.")
